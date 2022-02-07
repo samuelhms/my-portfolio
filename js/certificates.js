9fd543b86c8f4716c12d4b6e98ddd565c5fc79bb
samuel <samuel>
2022-02-06 21:37:07 -0300

    certificates poo

--- js/certificates.js
+++ js/certificates.js
@@ -21,6 +21,7 @@ certificates_url.push({ url: `images/portfolio/google/introainterview.jpg`, stac
 certificates_url.push({ url: `images/portfolio/outros/21443-95211sap.jpg`, stack: `all others` });
 certificates_url.push({ url: `images/portfolio/outros/28277_124743pdi.jpg`, stack: `all  others` });
 certificates_url.push({ url: `images/portfolio/outros/csharp-bradesco.jpg`, stack: `all  backend` });
+certificates_url.push({ url: `images/portfolio/outros/poo-bradesco.jpg`, stack: `all  backend` });
 
 
 function sortearCertificates() {
